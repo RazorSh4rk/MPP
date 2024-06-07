d22475b6ca3ef1c1826eb14e715fccc6ea489f91
oppakuu <bffkirilin@gmail.com>
2024-06-07 13:52:33 -0500

    All Books Page

--- src/librarysystem/AllBooks.java
+++ src/librarysystem/AllBooks.java
@@ -1,17 +1,17 @@
 package librarysystem;
 
-import business.ControllerInterface;
-import business.SystemController;
+import business.*;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,17 +19,24 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 
 import business.ControllerInterface;
 import business.SystemController;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessFacade;
 
 public class AllBooks extends JPanel {
     private static final long serialVersionUID = 1L;
     ControllerInterface ci = new SystemController();
 
+    JLabel errorField = new JLabel("$", JLabel.CENTER);
     private JPanel mainPanel;
     private JPanel topPanel;
+    private JPanel midPanel;
     private JPanel middlePanel;
+    private JPanel bottomPanel;
+    private JTextArea detailField = new JTextArea();;
     private TextArea textArea;
     public JPanel getMainPanel() {
         return mainPanel;
@@ -38,47 +45,116 @@ public class AllBooks extends JPanel {
 
     //Singleton class
     public AllBooks() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
+        mainPanel = new JPanel(new BorderLayout(10, 10));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         defineTopPanel();
         defineMiddlePanel();
+        defineBottomPanel();
         mainPanel.add(topPanel, BorderLayout.NORTH);
-        mainPanel.add(middlePanel, BorderLayout.CENTER);
+
+        midPanel = new JPanel(new GridLayout(2, 1, 10, 10));
+        midPanel.add(middlePanel);
+        midPanel.add(bottomPanel);
+
+        mainPanel.add(midPanel, BorderLayout.CENTER);
         mainPanel.setVisible(true);
     }
 
     public void defineTopPanel() {
         topPanel = new JPanel();
-        JLabel AllIDsLabel = new JLabel("All Book IDs");
+        JLabel AllIDsLabel = new JLabel("All Books");
         Util.adjustLabelFont(AllIDsLabel, Util.DARK_BLUE, true);
         topPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        topPanel.add(AllIDsLabel);
+        topPanel.add(AllIDsLabel);;
     }
 
     public void defineMiddlePanel() {
-        middlePanel = new JPanel();
-        FlowLayout fl = new FlowLayout(FlowLayout.CENTER, 25, 25);
-        middlePanel.setLayout(fl);
-        textArea = new TextArea(8, 20);
-        populateTextArea();
-        middlePanel.add(textArea);
+        middlePanel = new JPanel(new BorderLayout());
+
+        String[] columnNames = {"ISBN", "Title", "Max Checkout Duration"};
+        DefaultTableModel booksTable = new DefaultTableModel(columnNames, 0);
+
+        JTable table = new JTable(booksTable) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; // This causes the table not to be editable
+            }
+        };
+
+        table.setPreferredScrollableViewportSize(new Dimension(460, 220));
+        table.setFillsViewportHeight(true);
+
+        JButton refreshButton = new JButton("Refresh");
+        refreshButton.setPreferredSize(new Dimension(20, 20));
+
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) { // double-click
+                    int row = table.getSelectedRow();
+                    String isbn = (String) table.getValueAt(row, 0);
+                    showMemberDetails(isbn);
+                }
+            }
+        });
+
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        tableScrollPane.setPreferredSize(new Dimension(480, 230));
+
+        middlePanel.add(tableScrollPane, BorderLayout.CENTER);
+        middlePanel.add(refreshButton, BorderLayout.NORTH);
+        refreshButton.addActionListener(e -> updateTable(booksTable));
+        updateTable(booksTable);
+        middlePanel.setMinimumSize(new Dimension(460, 200));
+        middlePanel.setPreferredSize(new Dimension(460, 260));
+        middlePanel.setMaximumSize(new Dimension(460, 280));
 
     }
 
+    private void defineBottomPanel() {
+        bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
 
-    private void setData(String data) {
-        textArea.setText(data);
+        JPanel errorPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        errorPanel.add(errorField);
+        JPanel detailPanel = new JPanel(new BorderLayout());
+        detailField.setEditable(false);
+        detailPanel.add(detailField);
+        bottomPanel.add(detailPanel, BorderLayout.NORTH);
+        bottomPanel.add(errorPanel, BorderLayout.SOUTH);
     }
 
-	public void populateTextArea() {
-        //populate
-        List<String> ids = ci.allBookIds();
-        Collections.sort(ids);
-        StringBuilder sb = new StringBuilder();
-        for (String s : ids) {
-            sb.append(s + "\n");
+    private void updateTable(DefaultTableModel tableModel) {
+        tableModel.setRowCount(0);
+        DataAccess da = new DataAccessFacade();
+        Collection<Book> books = da.readBooksMap().values();
+
+        for (Book book : books) {
+            tableModel.addRow(new Object[]{book.getIsbn(),
+                    book.getTitle(),
+                    book.getMaxCheckoutLength()});
         }
-        setData(sb.toString());
+        tableModel.fireTableDataChanged();
+    }
+
+    private void showMemberDetails(String isbn) {
+        Book book = ci.getBook(isbn);
+
+        JPanel detailPanel = new JPanel();
+        detailPanel.setSize(460,300);
+        detailPanel.setLayout(new BorderLayout());
+
+        JTextArea detailArea = new JTextArea();
+        detailArea.setEditable(false);
+        detailArea.append("ISBN: " + book.getIsbn() + "\n");
+        detailArea.append("Title: " + book.getTitle() + "\n");
+        for(Author author: book.getAuthors()) {
+            detailArea.append("Author: " + author.getFirstName() + " " + author.getLastName() + "\n");
+        }
+        detailArea.append("Max Checkout Duration: " + book.getMaxCheckoutLength() + "\n");
+        detailArea.append("Number of copies: " + book.getCopyNums().size() + "\n");
+
+        detailField.setText(detailArea.getText());
     }
 }
 
